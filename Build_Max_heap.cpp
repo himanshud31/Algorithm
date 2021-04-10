089d428add6f78eaf256e905e2dd20ba1cc7b1f6
Himanshu Dhawan <56366633+himanshud31@users.noreply.github.com>
2021-04-10 13:08:19 +0530

    Sorting

---
+++ Build_Max_heap.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+void Max_heapify(int *arr,int i,int n)
+{
+	int largest=i;
+	if((2*i+1)<n and arr[largest]<arr[2*i+1])
+	largest=2*i+1;
+	if((2*i+2)<n and arr[largest]<arr[2*i+2])
+	largest=2*i+2;
+	
+	
+	
+	if(largest!=i)
+	{
+		swap(&arr[largest],&arr[i]);
+		Max_heapify(arr,largest,n);
+	}
+}
+void build_heap(int *arr,int n)
+{
+	for(int i=n/2;i>=0;i--)
+	{
+		Max_heapify(arr,i,n);
+	}
+}
+void display(int *arr,int n)
+{
+	for(int  i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int arr[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		int x;
+		cin>>x;
+		arr[i]=x;
+	}
+	
+	build_heap(arr,n);
+	display(arr,n);
+}
